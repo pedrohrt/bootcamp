8b07c49438416e893be4fcba982b97ab28232a01
pedrohrt <pedrohenrique.rodriguest@gmail.com>
2019-10-29 22:36:17 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express'; // importar o express
+import routes from './routes'; // exporta as rotas do arquivo routes.js
+
+class App {
+  constructor() {
+    // Metodo construtor, chamado automaticameente
+    this.server = express(); // instanciar o express na variavel server
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json()); // conifgura a aplicação para receber informacaoes no formato JSON
+  }
+
+  routes() {
+    this.server.use(routes); // usando as rotas de routes.js
+  }
+}
+
+export default new App().server; // exportando apenas a variavel server
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express'; // exporta somente o Router
+
+const routes = new Router();
+
+export default routes;
